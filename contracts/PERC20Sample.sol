cdfe06b623562200179a8c251a35223599dfbe56
conchitariveraellist7je <conchitariveraellist7je6@hotmail.com>
2024-09-20 16:27:27 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("e4a10af7b8eb06d7655bd189972bd9a2f9f3e4e0ee9baca3527d34e1c6b326ac", "e4a10af7b8eb06d7655bd189972bd9a2f9f3e4e0ee9baca3527d34e1c6b326ac") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
